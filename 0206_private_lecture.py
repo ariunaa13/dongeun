35f9884f776c92887c838ef168a95d245cd02399
ariunaa13 <dongeun13@gmail.com>
2020-02-10 21:20:51 +0900

    2월 10일 과제
    지니 완성

---
+++ 0206_private_lecture.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+musics = soup.select('#body-content > div > div > table > tbody > tr')
+rank = 1
+
+
+for music in musics:
+    title_tag = music.select_one('td.info > a.title.ellipsis')
+    singer_tag = music.select_one('td.info > a.artist.ellipsis')
+    title = title_tag.text
+    singer = singer_tag.text
+    #print(rank, title.strip(), singer)
+
+    doc = {
+        'rank' : rank,
+        'title' : title,
+        'singer' : singer
+    }
+
+    db.musics.insert_one(doc)
+    rank += 1
+    #
+    # doc = {
+    #     'rank' : rank,
+    #     'title' : title,
+    #     'singer' : singer
+    # }
+
+    # db.musics.insert_one(doc)
+    # rank += 1
